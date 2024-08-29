17bffbfc7ca624c1d33080b4a3048a8340c3c02f
AlbinEldhose <147093724+AlbinEldhose@users.noreply.github.com>
2024-08-29 23:01:13 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,16 +20,16 @@ module tt_um_arithmetic_logic_unit (
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-    assign uo_out[7:6]=0
+    assign uo_out[7:6]=0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n};
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
     arithmetic_logic_unit (
-        .result(uo_out[5:0]),
         .A(ui_in[2:0]),
         .B(ui_in[5:3]),
-        .sel(ui_in[7:6])
+        .sel(ui_in[7:6]),
+        .result(uo_out[5:0])
     );
 
 endmodule
